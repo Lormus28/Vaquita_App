edc3b0ad52c0442f7d5c177ea64ba075bff66d6b
laura.posadaaz@gmail.com <you@example.com>
2024-05-09 19:16:01 -0500

    creating dotenv for the proyect

--- back_end/app.js
+++ back_end/app.js
@@ -1,11 +1,16 @@
 import express from "express";
+import "dotenv/config";
 import cors from "cors";
+import groupRouter from "./src/routers/groups.routers.js";
 
 const app = express();
 
 
 app.use(express.json());
 app.use(cors());
+
+//Es necesario configurar las rutas para que la app las pueda leer y utilizar
+app.use('/groups', groupRouter());
 
 const PORT = process.env.PORT || 3000;
 
---
+++ back_end/src/routers/groups.routers.js
@@ -0,0 +1,12 @@
+import Router from "express-promise-router";
+
+const GroupsRouter = () => {
+
+    const router = Router();
+
+    router.get('/', () => {});
+
+    return router;
+};
+
+export default GroupsRouter;
